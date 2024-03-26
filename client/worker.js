286fa41a67976357708ba0a9e6a0036d2aadcb3b
iliasabai123 <ilias.abai.alser@gmail.com>
2024-03-26 17:22:23 +0600

    пушка готова

--- client/worker.js
+++ client/worker.js
@@ -4,7 +4,41 @@ self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Traversy Media!",
-    icon: "http://image.ibb.co/frYOFd/tmlogo.png"
+    // Текс пуш уведомления
+    body: data.body || "",
+    // Иконка пуш уведомления
+    icon: data.icon || "",
+    // Фотка пуш уведомления
+    image: data.image,
+    // Указывает на то что экраны с достаточно большим дисплеем можно было чтобы пушка не пропадала автоматом
+    requireInteraction: data.requireInteraction || false,
+    // Указывает на то что пушка будет без звука
+    silent: data.silent,
+    // Указывает на то что пугка если еще стоит то можно по идентификатору отправить другую пушку и заменить текущую
+    tag: data.tag || "",
+    // Принимает массив обьектов с полями action - название события title - имя события icon - иконка события кнопки
+    // Потом эти кнопки можно будет отработать в событий notificationclick в сервисном работнике
+    actions: data.actions || null,
+    // Можно отработать передаваемые данные
+    data: data.data,
+    renotify: data.renotify || false,
+    // timestamp: data.timestamp || null,
+    // badge: data.badge || '',
+    // dir: data.dir || '',
+    // lang: data.lang,
+    // vibrate: data.vibrate || [100],
   });
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close(); // Закрыть уведомление
+
+  // Получить URL из свойств data уведомления
+  const url = event.notification.data.url;
+
+  // Открыть ссылку в новой вкладке
+  event.waitUntil(
+      clients.openWindow(url)
+  );
 });
--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const path = require("path");
+const axios = require("axios");
 
 const app = express();
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Разрешить запросы с любых источников
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Разрешить указанные методы запросов
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Разрешить указанные заголовки
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Для предварительных запросов (OPTIONS) возвращаем успешный статус без тела ответа
+  } else {
+    next(); // Передаем управление следующему middleware в цепочке
+  }
+});
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
 
@@ -15,28 +25,54 @@ const publicVapidKey =
 const privateVapidKey = "lcjLMiOQSo6bXlVvB8A8d-79bPqQkzN840FZ1F1wc3Q";
 
 webpush.setVapidDetails(
-  "ilias.abai.alser@gmail.com",
-  publicVapidKey,
-  privateVapidKey
+    "mailto:ilias.abai.alser@gmail.com",
+    publicVapidKey,
+    privateVapidKey
 );
 
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
-  // Get pushSubscription object
-  const subscription = req.body;
-
-  // Send 201 - resource created
+  const pushConfig = req.body;
+  axios.put(`https://phrases-ae88c-default-rtdb.firebaseio.com/users/${pushConfig.keys.auth}.json`, pushConfig)
   res.status(201).json({});
-
-  // Create payload
-  const payload = JSON.stringify({ title: "Push Test" });
-
-  // Pass object into sendNotification
-  webpush
-    .sendNotification(subscription, payload)
-    .catch(err => console.error(err));
 });
 
-const port = 5000;
+
+app.post("/push", async (req, res) => {
+  try {
+    const subscription = req.body.pushConfig;
+    const pushParams = req.body.pushParams;
+    res.status(201).json({});
+    const payload = JSON.stringify(pushParams);
+    await webpush
+        .sendNotification(subscription, payload)
+  } catch (e) {
+    res.status(503).json({e});
+    res.send('error')
+  }
+});
+
+app.post("/pushMany", async (req, res) => {
+  try {
+    const body = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify(body);
+    const resp = await axios.get('https://phrases-ae88c-default-rtdb.firebaseio.com/users.json');
+    for (const respKey in resp.data) {
+      try {
+        await webpush
+            .sendNotification({...resp.data[respKey], expirationTime: null}, payload)
+      } catch (e) {
+        console.log('eeee', e);
+      }
+
+    }
+  } catch (e) {
+    res.status(503).json({e});
+    res.send('error')
+  }
+});
+
+const port = 3333;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
